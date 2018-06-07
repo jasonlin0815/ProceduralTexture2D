57c7af8ac39b6fa0f4c68a6a06524e59c2ec87f9
Jason Lin <jlin116@ucsc.edu>
2018-06-07 12:35:27 -0700

    Update gitignore path and commit default project setup

---
+++ Assets/Scripts/TextureGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private int width = 1920;
+
+    [SerializeField]
+    private int height = 1080;
+
+    [SerializeField]
+    private int resolution = 1000;
+
+    Texture2D texture;
+
+    private void Awake()
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            Debug.Log(Perlin.Noise((float)x / (float)resolution));
+        }
+    }
+}
