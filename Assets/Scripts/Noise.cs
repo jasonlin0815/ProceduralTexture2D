844561f5e5bef201549959fc810d8a78e4b9333b
Jason Lin <jlin116@ucsc.edu>
2018-06-07 19:00:53 -0700

    Texture generation with movement and animation speed variation

---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise
+{
+    public static float[] GenerateNoiseMap(int width, int height, float scale, int octaves, float persistance, float lacunarity, float movement, float animateDelta)
+    {
+        // Allocate space to generate noise map containing the terrain height at x coordinate
+        float[] noiseMap = new float[width];
+
+        // Adjust scale if input is invalid
+        if (scale <= 0)
+        {
+            scale = 0.0001f;
+        }
+
+        // Loop through every pixel of width
+        for (int w = 0; w < width; ++w)
+        {
+            // Amplitude, frequency and noise height are used to add noises layers
+            float amplitude = 1;
+            float frequency = 1;
+            float noiseHeight = 0;
+
+            // Each octaves will add an extra layer to the overall noise, with different amplitude and frequency sampling
+            for (int i = 0; i < octaves; ++i)
+            {
+                float sampleWidth = (w + movement) / scale * frequency;
+                float noiseValue = Mathf.PerlinNoise(sampleWidth, animateDelta);
+
+                noiseHeight += noiseValue * amplitude;
+                amplitude *= persistance;
+                frequency *= lacunarity;
+            }
+
+            // Normalize the noiseHeight back to [0, 1] scale
+            noiseMap[w] = noiseHeight / octaves * height;
+        }
+
+        return noiseMap;
+    }
+}
--- Assets/Scripts/TextureGenerator.cs
+++ Assets/Scripts/TextureGenerator.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Linq;
 
 public class TextureGenerator : MonoBehaviour
 {
@@ -11,15 +13,93 @@ public class TextureGenerator : MonoBehaviour
     private int height = 1080;
 
     [SerializeField]
-    private int resolution = 1000;
+    private Color terrainColor;
 
-    Texture2D texture;
+    [Range(0, 10)]
+    [SerializeField]
+    private float moveSpeed;
+
+    [Range(0, 10)]
+    [SerializeField]
+    private float animateSpeed;
+
+    [Range(1, 5)]
+    [SerializeField]
+    private int octaves = 3;
+
+    [Range(0, 1)]
+    [SerializeField]
+    private float persistance;
+
+    [Range(1, 2)]
+    [SerializeField]
+    private float lacunarity;
+
+    [Range(1, 1000)]
+    [SerializeField]
+    private float scale;
+
+    private Texture2D texture;
+    private SpriteRenderer spriteRenderer;
+    private float movement = 0;
+    private float timeLapse = 0;
 
     private void Awake()
     {
-        for (int x = 0; x < width; ++x)
+        // Create a Texture2D object with given width and height
+        texture = new Texture2D(width, height);
+
+        // Generate the texture with time = 0
+        Clear();
+        Generate(0, 0);
+        Apply();
+    }
+
+    private void Update()
+    {
+        movement += Time.deltaTime * moveSpeed;
+        timeLapse += Time.deltaTime * animateSpeed;
+        Clear();
+        Generate(movement, timeLapse);
+        Apply();
+    }
+
+    public void Clear()
+    {
+        // Fill the texture with transparent color
+        // Traverse using the return texture width and height from Texture2D.blackTexture
+        Texture2D blackTexture = Texture2D.blackTexture;
+        for (int w = 0; w < texture.width;)
         {
-            Debug.Log(Perlin.Noise((float)x / (float)resolution));
+            for (int h = 0; h < texture.height;)
+            {
+                texture.SetPixels32(w, h, blackTexture.width, blackTexture.height, blackTexture.GetPixels32());
+                h += blackTexture.height;
+            }
+            w += blackTexture.width;
         }
+        texture.Apply();
+    }
+
+    public void Generate(float movement, float time)
+    {
+        // Generate Noise to create terrrain line
+        float[] heightmap = Noise.GenerateNoiseMap(width, height, scale, octaves, persistance, lacunarity, movement, time);
+        for (int w = 0; w < width; ++w)
+        {
+            texture.SetPixels(w, 0, 1, (int)heightmap[w], Enumerable.Repeat<Color>(terrainColor, (int)heightmap[w]).ToArray());
+        }
+    }
+
+    public void Apply()
+    {
+        // Apply the pixel changes to texture
+        texture.Apply();
+
+        // Apply the texture to the sprite renderer by converting the Texture2D to Sprite object
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+
+        spriteRenderer.sprite = sprite;
     }
 }
